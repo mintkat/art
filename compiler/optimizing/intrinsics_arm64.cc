43b2b9c700db95f1157de7255a5bb1a647668c58
Roland Levillain <rpl@google.com>
2016-01-12 12:01:04 +0000

    Fix memory fences in the ARM64 UnsafeCas intrinsics.
    Also add some comments for the ARM UnsafeCas intrinsics.
    
    Change-Id: Ic6e4f2c37e468db4582ac8709496a80f3c1f9a6b

--- compiler/optimizing/intrinsics_arm64.cc
+++ compiler/optimizing/intrinsics_arm64.cc
@@ -881,7 +881,11 @@ static void GenCas(LocationSummary* locations, Primitive::Type type, CodeGenerat
     __ Stlxr(tmp_32, value, MemOperand(tmp_ptr));
     __ Cbnz(tmp_32, &loop_head);
   } else {
-    __ Dmb(InnerShareable, BarrierWrites);
+    // Emit a `Dmb(InnerShareable, BarrierAll)` (DMB ISH) instruction
+    // instead of a `Dmb(InnerShareable, BarrierWrites)` (DMB ISHST)
+    // one, as the latter allows a preceding load to be delayed past
+    // the STXR instruction below.
+    __ Dmb(InnerShareable, BarrierAll);
     __ Bind(&loop_head);
     __ Ldxr(tmp_value, MemOperand(tmp_ptr));
     __ Cmp(tmp_value, expected);
